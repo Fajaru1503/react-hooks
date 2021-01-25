4d5d785e5a925a9deaa364acb6c930c25d70bef3
Fajaru1503 <fajarudewangga@gmail.com>
2021-01-25 12:53:14 +0800

    converting App Component to function base

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
+import Header from "./components/Header/Header";
+import Auth from "./components/Auth/Auth";
 import Todo from "./components/Todo/Todo";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Todo />
-      </div>
-    );
-  }
-}
+const app = (props) => {
+  const [page, setPage] = useState("auth");
 
-export default App;
+  const switchPage = (pageName) => {
+    setPage(pageName);
+  };
+
+  return (
+    <div className="App">
+      <Header
+        onLoadTodos={switchPage.bind(this, "todos")}
+        onLoadAuth={switchPage.bind(this, "auth")}
+      />
+
+      <hr />
+
+      {page === "auth" ? <Auth /> : <Todo />}
+    </div>
+  );
+};
+
+export default app;
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Auth = (props) => <h1>Authentication Component</h1>;
+
+export default Auth;
