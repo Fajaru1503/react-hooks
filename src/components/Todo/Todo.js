a9fce3385dfa5e01dceed90267eb8714071f1a4a
Fajaru1503 <fajarudewangga@gmail.com>
2021-01-25 12:01:24 +0800

    understanding using hooks as object type

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -4,12 +4,23 @@ const Todo = (props) => {
   const [todoName, setTodoName] = useState("");
   const [todoList, setTodoLIst] = useState([]);
 
+  // other way to using Hooks
+  // const [todoState, setTodoState] = useState({ userInput: "", todoList: [] });
+
   const inputChangeHandler = (event) => {
     setTodoName(event.target.value);
+    // setTodoState({
+    //   userInput: event.target.value,
+    //   todoList: todoState.todoList,
+    // });
   };
 
   const submitTodoHandler = () => {
     setTodoLIst(todoList.concat(todoName));
+    // setTodoState({
+    //   userInput: todoState.userInput,
+    //   todoList: todoState.todoList.concat(todoState.userInput),
+    // });
   };
 
   return (
